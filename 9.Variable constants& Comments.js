1ac866d0db477430394e751fda8b3cc673d3dc03
Rajesh39996 <rajesh39996@gamil.com>
2026-03-03 10:05:45 +0530

    first Push to git

---
+++ 9.Variable constants& Comments.js
@@ -0,0 +1,20 @@
+let age=25;
+console.log(age);
+
+const score=10
+console.log(score);
+
+age=35
+console.log(age)
+
+// score=40
+// console.log(score)
+
+var year=2026
+console.log(year)
+
+//single line comment
+/* multiline comment
+second line*/
+
+//string
