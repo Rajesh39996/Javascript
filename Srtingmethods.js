1f06249d78b76daafc796b5cbf723e72a20c09e8
Rajesh39996 <rajesh39996@gamil.com>
2026-03-03 21:26:35 +0530

    String Methods

---
+++ Srtingmethods.js
@@ -0,0 +1,41 @@
+let firstName="Rajesh Kumar";
+let lastName="Reddy";
+//let work=`it`s my job`;
+let fullName=firstName+lastName;
+console.log(fullName);
+console.log(firstName,lastName,fullName);
+console.log(firstName," ",lastName);
+console.log()
+
+console.log(firstName+lastName);
+
+//Acessing characters/indexing
+console.log(firstName[0]);
+console.log(firstName[0,8]);
+
+//Properties
+console.log(firstName.length);
+console.log(lastName.length);
+
+//Methods
+let random=firstName.toUpperCase();
+console.log(random);
+console.log(firstName);
+console.log(lastName.toLowerCase());
+
+//13.String Methods
+console.log(firstName.startsWith("R"));
+console.log(firstName.startsWith("r"));
+console.log(firstName.endsWith("r"));
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.indexOf("a"));
+console.log(firstName.lastIndexOf("a"));
+console.log(firstName.replace("a","-"));
+console.log(firstName.replaceAll("a","-"));
+console.log(firstName.substring(0,8));
+console.log(firstName.substring(0,20));
+console.log(firstName.slice(0,8));
+console.log(firstName.includes("Raj"));
+console.log(firstName.includes("raj"));7
+
---
+++ practice.js
@@ -0,0 +1,11 @@
+//string methods
+let firstName="Rajesh Kumar"
+let lastName='Reddy'
+//let a=`it`s my job
+//console.log(firstName);
+//console.log(firstName+" "+  lastName);
+let fullName= firstName+lastName;
+console.log(firstName.slice(0,8));
+console.log(firstName.indexOf("a"));
+console.log(firstName.lastIndexOf("a"));
+
