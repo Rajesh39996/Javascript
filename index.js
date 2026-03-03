e84bf5da2ef1aa55b67c274d2fa252d8701d01c7
Rajesh39996 <rajesh39996@gamil.com>
2026-03-03 21:57:42 +0530

    numbers

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //Numbers
+let radius=15;
+let pi=3.14;
+//addition of two Numbers
+let result= radius+pi;
+console.log(result);
+//Subration of two Numbers
+let result1= radius-pi;
+console.log(result1);
+//Mulitiplitions of two numbers
+let result2=radius*pi;
+console.log(result2);
+
+let result3=radius**pi;
+console.log(result3)
+
+let result4=radius/pi;
+console.log(result4)
+//area
+let area=pi+radius**2;
+console.log(area);
+
+let area1=3.14+15**2;
+console.log(area1);
+
+let a= 15*(20-18)**3;
+console.log(a);
