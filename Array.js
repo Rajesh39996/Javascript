8bfd6c732f593b544e29242c1e50d58b2f092b5b
Rajesh39996 <rajesh39996@gamil.com>
2026-03-04 22:39:10 +0530

    Arrays methods added

--- Array.js
+++ Array.js
@@ -1,6 +1,35 @@
 let products=["bread","butter","salt","jam"];
 console.log(products);
 console.log(products[0]);
-console.log(products.join("-"))
-console.log(prodicts.join(" "))
-console.log(products.join(", "))
+console.log(products.join("-"));
+console.log(products.join(" "));
+console.log(products.join(", "));
+products[3]="Pizza";
+console.log(products);
+let random="xyz";
+random[1]="q";
+console.log(random);
+
+let items=["bread","butter","salt","jam"];
+let update=["water","pizza"];
+itemsMethod=items.join(", ");
+console.log(itemsMethod)
+
+itemsitems=items.concat(update)
+console.log(itemsitems)
+
+itemsadding=items.concat(["tissues","coke"]);
+console.log(itemsadding);
+
+let a=["bread","butter","salt","jam",20]
+amethod=a.indexOf(20);
+console.log(amethod)
+
+let things=["bread","butter","salt","jam"];
+thingsmethod=things.push("water");
+console.log(thingsmethod)
+console.log(things)
+
+thingsmethods1=things.pop("water");
+console.log(thingsmethods1)
+console.log(things)
