58e6d4297d4447bd2bf290d96cd8c2d69f950196
Rajesh39996 <rajesh39996@gamil.com>
2026-03-04 22:08:29 +0530

    arrays

---
+++ Array.js
@@ -0,0 +1,6 @@
+let products=["bread","butter","salt","jam"];
+console.log(products);
+console.log(products[0]);
+console.log(products.join("-"))
+console.log(prodicts.join(" "))
+console.log(products.join(", "))
