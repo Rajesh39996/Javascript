0c6b787e3497bfcd6cab9c77b1614c5f79dea151
Rajesh39996 <rajesh39996@gamil.com>
2026-03-05 16:38:03 +0530

    typeconversion

---
+++ type_conversion.js
@@ -0,0 +1,44 @@
+let random="25"
+console.log(typeof random);
+
+let age=Number(random);
+console.log(age);
+
+let year=1996;
+console.log(typeof year);
+
+let yearString=String(year);
+console.log(yearString);
+console.log(typeof yearstring)
+
+let random1="xyz";
+console.log(Number(random1));
+
+let random2="xyz";
+let value=Number(random2);
+console.log(value);
+console.log(typeof value);
+
+let result=Boolean(50);
+console.log(result);
+
+let result1=Boolean("");
+console.log(result1);
+
+let result2=Boolean(" ");
+console.log(result2);
+
+let result3=Boolean(true);
+console.log(result3);
+
+let result4=Boolean(false);
+console.log(result4);
+
+let result5=Boolean(0);
+console.log(result5);
+
+let result6=Boolean("xyz");
+console.log(result6);
+
+
+
