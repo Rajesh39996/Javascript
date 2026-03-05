88699cda4dd75a26c5522eb7dcaa1d13f2442c88
Rajesh39996 <rajesh39996@gamil.com>
2026-03-05 10:01:12 +0530

    null and undefined

---
+++ null_undefined.js
@@ -0,0 +1,10 @@
+//Null and Undefined
+let score =7
+
+let update=null;
+console.log(update);//null
+console.log(score+update);//7
+
+let year;
+console.log(year);//undefined
+console.log(score+year);//NAN
