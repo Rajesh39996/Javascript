2110e8d3c0d8c04e711aa5bc78763acd3fd53c56
Rajesh39996 <rajesh39996@gamil.com>
2026-03-05 15:34:27 +0530

    looseandstrict

---
+++ loose_vs_strict.js
@@ -0,0 +1,8 @@
+let age=20;
+console.log(age==25);
+console.log(age=="20");
+console.log(age==="20");
+console.log(age==20);
+console.log(age!="25");
+console.log(typeof age);
+console.log(typeof "25")
