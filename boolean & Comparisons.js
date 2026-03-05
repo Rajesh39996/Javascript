bd51adb144246d787d0409e0dbe83a3b22b90d4e
Rajesh39996 <rajesh39996@gamil.com>
2026-03-05 15:15:33 +0530

    newpush

---
+++ boolean & Comparisons.js
@@ -0,0 +1,26 @@
+let value=[true,false,"trueajld","falsesljf"];
+console.log(value)
+
+let values=[true,false,"true","false"];
+console.log(values);
+
+let email="rajesh39996@gmail.com";
+console.log(email.includes("Rajesh"));
+
+let email1="rajesh39996@gmail.com";
+console.log(email1.includes("rajesh"));
+
+let students=["rajesh","ramesh","rakesh"];
+console.log(students.includes("uday"));
+
+let age=25;
+console.log(age==25);
+console.log(age==35);
+console.log(age!=50);
+console.log(age>20);
+console.log(age<20);
+console.log(age<=25);
+console.log(age>=25);
+console.log(age<=26);
+console.log(age>=24);
+console.log("abc">"xyz");
