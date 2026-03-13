9de8f3d03d42357e5ec063d3892ed7f364639a37
Rajesh39996 <rajesh39996@gamil.com>
2026-03-13 18:04:56 +0530

    nicework

---
+++ else_elseif.js
@@ -0,0 +1,91 @@
+let score=5
+if (score>7){
+    console.log("good job!")
+}else{
+    console.log("better luck next time")
+}
+
+const loggedIn=true
+if (loggedIn){
+    console.log("welcome")
+}else{
+    console.log("please loggin now")
+}
+
+const sigin=""
+if (sigin){
+    console.log("welcome")
+}else{
+    console.log("please sigin First")
+}
+
+const accessaccount="xyz"
+if (accessaccount){
+    console.log("Hello!, Welcome")
+}else{
+    console.log("please access your account first!")
+}
+
+const authenticate=0;
+if(authenticate){
+    console.log("Welcome, User!")
+}else{
+    console.log("please login user!")
+}
+
+const authentication=null
+if(authentication){
+    console.log("your in!")
+}else{
+    console.log("your out!")
+}
+
+const Memberlogin=5
+if(Memberlogin){
+    console.log("Glad to see you")
+}else{
+    console.log("please login the account")
+}
+
+let signal="red"
+if(signal=="red"){
+    console.log("STOP")
+}else{
+    console.log("Goooooooooo!")
+}
+
+let sign="green"
+if(sign=="red"){
+    console.log("Please STOP")
+}else if(sign=="yello"){
+    console.log("please be Ready!")
+}else if(sign=="green"){
+    console.log("gooooooooooo!")
+}else{
+    console.log("please check the condition")
+}
+
+let total=10
+if(total==10){
+    console.log("Great work!")
+}else if(total==9){
+    console.log("soo close!")
+}else if(total==8){
+    console.log("Great work, keep going")
+}else if(total==7){
+    console.log("Just a littel brush up")
+}else if(total==6){
+    console.log("you need pratice")
+}else if(total==5){
+    console.log("hmm, please")
+}else if(total==4){
+    console.log("you need focuse")
+}else if(total==3){
+    console.log("you need focuse")
+}else if(total==2){
+    console.log("you need focuse")
+}else if (total==1){
+    console.log("you need focuse!")
+}else{
+    console.log("invalid score")
+}
