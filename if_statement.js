a6f7f43785c43a7e1c9513990e4eb4dd5d911cbf
Rajesh39996 <rajesh39996@gamil.com>
2026-03-13 17:29:29 +0530

    new commit 1

---
+++ if_statement.js
@@ -0,0 +1,27 @@
+let score=9;
+if(score>7){
+    console.log("rajesh")
+}
+console.log("out of condition")
+
+let number=7
+if(number>=7){
+    console.log("hello world")
+}
+
+let total=7
+if (total<=7){
+    console.log("Total")
+}
+
+let num=7
+if(num>7){
+    console.log("hi")
+}
+
+
+const students=["rajesh","hello","hi","total","count","digits","value","quandity","Amount","integer","sum"]
+if(students.length>0){
+    console.log("we have a class today")
+}
+
