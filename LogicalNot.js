8ddd59773e64120a6540795094176f1cce9e14e1
Rajesh39996 <rajesh39996@gamil.com>
2026-03-13 20:11:43 +0530

    logicalNot

---
+++ LogicalNot.js
@@ -0,0 +1,19 @@
+console.log(true);
+console.log(!true);
+
+console.log(false);
+console.log(!false);
+
+let loggin=true
+if(loggin){
+    console.log("welcome user")
+}else{
+    console.log("please login")
+}
+
+let sigin=0
+if(!sigin){
+    console.log("welcome user")
+}else{
+    console.log("please login")
+}
