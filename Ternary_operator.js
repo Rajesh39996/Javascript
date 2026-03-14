3b77e51e72a9a63747da6302afccaf4a7ce42cf4
Rajesh39996 <rajesh39996@gamil.com>
2026-03-14 14:03:00 +0530

    Ternary_operator

---
+++ Ternary_operator.js
@@ -0,0 +1,25 @@
+// const user=true;
+// if(user){
+//     console.log("true Value")
+// }else{
+//     console.log("false value")
+// }
+
+// const result=10>2
+// if (result){
+//     console.log("true value")
+// }else{
+//     console.log("flase value")
+// }
+
+// const user1=true;
+// user?console.log("true value"):console.log("false value")
+
+// const user2="rajesh"
+// user.length>5? console.log("Rajesh Kumar"):console.log("rajesh kumar reddy")
+
+const user3="rajesh"
+const result1= user3.length>=5? user3.length:0;
+console.log(result1)
+
+
--- scope_Global_Local.js
+++ scope_Global_Local.js
@@ -1,8 +1,21 @@
 let score=70;
-if (score>80){
-    console.log("inside<80block");
-    if (score>65){
-        console.log("indise>65block")
-    }
-}
+// if (score<80){
+//     console.log("inside<80block");
+//     if (score>65){
+//         console.log("indise>65block")
+//     }
+// }
 console.log("inside Globel Block")
+
+if (score<80){
+    var in80=80;
+    console.log("Inside <80 Block",score,in80);
+        if(score>65){
+            let in65=65;
+        console.log("inside>65 Block",score,in65);
+            if (score>=70){
+                console.log(in80,in65)
+            }
+        }
+}
+console.log("insideglobalblock",score,in80)
