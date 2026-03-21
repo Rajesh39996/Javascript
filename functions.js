74cc98e5891e39dbcec0f02ee31702e13752039c
Rajesh39996 <rajesh39996@gamil.com>
2026-03-21 14:52:27 +0530

    hello

---
+++ functions.js
@@ -0,0 +1,10 @@
+function addition(numone, numtwo){
+    return numone+numtwo
+}
+let result= addition(2,5)
+console.log(result)
+
+function add(numone, numtwo){
+    return numone+numtwo
+}
+console.log(add(1,2))
---
+++ functions2.js
@@ -0,0 +1,17 @@
+// function intro(){
+//     console.log("Welcome!")
+// }
+// intro();
+
+
+// let intro1=function(){
+//     console.log("welcome the World!")
+// }
+// intro1()
+// console.log(intro1())
+
+intro2()
+
+function intro2(){
+    console.log("welcome to myComputer")
+}
